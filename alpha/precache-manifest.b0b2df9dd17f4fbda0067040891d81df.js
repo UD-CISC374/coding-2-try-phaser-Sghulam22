c1556e5b274685c7ff3677a3f9509b16ef7b7256
Shabir Ghulam <sghulam@udel.edu>
2020-02-28 14:50:01 -0500

    Updates

---
+++ alpha/precache-manifest.b0b2df9dd17f4fbda0067040891d81df.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0691eaa31f4c895c103e3f89ea2d75",
+    "url": "/coding-2-try-phaser-Sghulam22/alphaassets/citations.md"
+  },
+  {
+    "revision": "1d60fcac2f6c24de41b791a3f043c118",
+    "url": "/coding-2-try-phaser-Sghulam22/alphaassets/jungle.jpg"
+  },
+  {
+    "revision": "1881d10833f1ba68cb9ae0368c0c7cf3",
+    "url": "/coding-2-try-phaser-Sghulam22/alphaassets/laser.png"
+  },
+  {
+    "revision": "835a5bebc26d3e41073700ea646b9cd6",
+    "url": "/coding-2-try-phaser-Sghulam22/alphaassets/player.png"
+  },
+  {
+    "revision": "e217cee6e6eaf232f6bd0e2cab85d632",
+    "url": "/coding-2-try-phaser-Sghulam22/alphaassets/space.jpg"
+  },
+  {
+    "revision": "3218c8ed7c3dae505321bb76830ce422",
+    "url": "/coding-2-try-phaser-Sghulam22/alphaassets/spaceship.png"
+  },
+  {
+    "revision": "4e9f0c3805399fe4de9c",
+    "url": "/coding-2-try-phaser-Sghulam22/alphagen.main.92ace3332cd06f1eb4d7.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-Sghulam22/alphagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "8aabc9b3c18d931d9ed34d42ed7a1078",
+    "url": "/coding-2-try-phaser-Sghulam22/alphaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-Sghulam22/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-Sghulam22/alphasw.js"
+  }
+]);
