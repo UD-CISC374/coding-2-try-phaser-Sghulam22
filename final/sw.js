6960f849f7eb38d5e8582dc6e80b89eb1cc24a5d
Shabir Ghulam <sghulam@udel.edu>
2020-02-28 14:46:34 -0500

    Updates

--- final/sw.js
+++ final/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/devprecache-manifest.549ef5a400021d602b1a18ffdc9f4103.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/floating_objects/finalprecache-manifest.a18efb68d2f4d1a8b9ef2e0cb237e546.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
---
+++ precache-manifest.fb247b357cd450f48bf3e36a6cef02c8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0691eaa31f4c895c103e3f89ea2d75",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "1d60fcac2f6c24de41b791a3f043c118",
+    "url": "assets/jungle.jpg"
+  },
+  {
+    "revision": "1881d10833f1ba68cb9ae0368c0c7cf3",
+    "url": "assets/laser.png"
+  },
+  {
+    "revision": "835a5bebc26d3e41073700ea646b9cd6",
+    "url": "assets/player.png"
+  },
+  {
+    "revision": "e217cee6e6eaf232f6bd0e2cab85d632",
+    "url": "assets/space.jpg"
+  },
+  {
+    "revision": "3218c8ed7c3dae505321bb76830ce422",
+    "url": "assets/spaceship.png"
+  },
+  {
+    "revision": "d15d040d3d4ae248a5a0c1ca377b1e53",
+    "url": "index.html"
+  },
+  {
+    "revision": "16cbb2728406ca996965",
+    "url": "main.19b14cbfe9a8b0348d71.bundle.js"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "sw.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "vendors.a047ee0740c7d21714d9.bundle.js"
+  }
+]);
