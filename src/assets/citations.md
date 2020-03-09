977aff207f37b31a6cd0ba296b7b971e88d98514
Shabir Ghulam <sghulam@udel.edu>
2020-03-09 19:27:44 -0400

    sound

--- src/assets/citations.md
+++ src/assets/citations.md
@@ -4,4 +4,5 @@ https://ya-webdesign.com/explore/energy-orb/
 https://www.pngguru.com/free-transparent-background-png-clipart-glxow/download
 background: https://www.freepik.com/premium-vector/jungle-game-background_5844339.html
 player: https://ya-webdesign.com/imgdownload.html
+sounds: https://www.zapsplat.com/
 *At least a URL, but also be sure to include any extra information that the artist/author requests you include.*
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -10,12 +10,10 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    //this.load.image("background","../src/assets/background.png")
     this.load.image("background","assets/jungle.jpg");
     this.load.image("player","assets/player.png");
     this.load.image("beam","assets/laser.png")
     this.load.image("spaceship","assets/spaceship.png");
-    //this.load.image("spaceship","assets/spaceship.png")
     this.load.audio("shot","assets/shot.mp3")
     this.load.audio("hit","assets/destroy.mp3")
 
